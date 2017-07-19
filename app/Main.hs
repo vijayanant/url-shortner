98076a710c06963ef6478239d5e7cb326135605a
Vijay Anant <vijay.hassan@gmail.com>
2017-07-19 18:17:52 +0530

    make sure same id is not generated again (even for same URI)

--- app/Main.hs
+++ app/Main.hs
@@ -2,21 +2,24 @@
 module Main where
 
 import IdGenerator
-import Data.ByteString.Char8  as BC (pack, unpack)
+import Data.ByteString.Char8  as BC (pack, unpack, concat)
 import Web.Scotty
-import Data.Text.Lazy as TL
+import Data.Text.Lazy as TL (pack, concat)
 import Data.Text.Lazy.Encoding as LE
 import Network.URI (parseAbsoluteURI)
 import Control.Monad.IO.Class
 
+import Data.Time.Clock.POSIX (getPOSIXTime)
+
+-- keep increasing this number occationally to (once a year?)
+deductSeconds = 1500000000 
 main :: IO ()
 main = scotty 8000 $ do 
-  get "/"  $ do
+  put "/"  $ do
+    currentTime <- liftIO $ round <$> getPOSIXTime
     uri <- param "uri"
-    case parseAbsoluteURI (TL.unpack uri) of
-      Just u -> do 
-        uriBS <- return $ BC.pack $ TL.unpack uri
-        code <- return $  shortId2ByteString $ generate (NodeId 1) uriBS  10000 
+    case parseAbsoluteURI (BC.unpack uri) of
+      Just _ -> do
+        code <- return $  shortId2ByteString $ generate (NodeId 1) uri  (currentTime - deductSeconds) 
         text $ TL.pack $ show  code
-        {-text $ TL.concat ["Invalid URI -  ", uri]-}
-      Nothing -> text $ TL.concat ["Invalid URI -  ", uri]
+      Nothing -> text $ TL.concat  [ "Invalid URI -  ", TL.pack $ show uri]
